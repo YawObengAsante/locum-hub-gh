8cf4aea832a9dfde4763ea90ac227111e2cbbc2f
Yaw Obeng Asante <yawobengasante@gmail.com>
2025-11-23 17:21:33 +0000

    (fix): fixed prisma errors

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,18 @@
-import { PrismaClient } from '@prisma/client'
+// import PrismaClient from '@prisma/client';
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+// const globalForPrisma = global as unknown as { prisma: Prisma }
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['query', 'info', 'warn', 'error'],
-    datasourceUrl: process.env.DATABASE_URL,
-  })
+// export const prisma =
+//   globalForPrisma.prisma ||
+//   new PrismaClient({
+//     log: ['query', 'info', 'warn', 'error'],
+//   })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '../generated/prisma/client'
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
+export const prisma = new PrismaClient({ adapter })

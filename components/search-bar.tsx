0ae1e8178307f20f22322cd3f0f85ece375f972c
Yaw Obeng Asante <yawobengasante@gmail.com>
2025-11-30 18:11:38 +0000

    (refactor): refactored search bar component

--- components/search-bar.tsx
+++ components/search-bar.tsx
@@ -4,24 +4,28 @@ import { JobTypeSelectGroup } from "./job-type-select-group";
 
 export default function SearchBar() {
   return (
-    <div className="grid gap-2.5 p-2 md:p-3 md:my-3 bg-white/70 rounded-2xl shadow-xl">
-      <h1 className="font-bold text-xs md:text-2xl">Find Jobs</h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-2.5">
-        <Input
-          type="search"
-          placeholder="Search Job"
-          className="bg-white"
-        />
-        <JobTypeSelectGroup name="jobType" />
-        <Input
-          type="search"
-          placeholder="Location"
-          className="bg-white "
-        />
+    <form action="/jobs">
+      <div className="grid gap-2.5 p-2 md:p-3 md:my-3 bg-white/70 rounded-2xl shadow-xl">
+        <h1 className="font-bold text-xs md:text-2xl">Find Jobs</h1>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-2.5">
+          <Input
+            type="search"
+            name="job"
+            placeholder="Search Job"
+            className="bg-white"
+          />
+          <JobTypeSelectGroup name="jobType" />
+          <Input
+            type="search"
+            name="location"
+            placeholder="Location"
+            className="bg-white "
+          />
+        </div>
+        <Button type="submit" className="text-white">
+          Search
+        </Button>
       </div>
-      <Button className="text-white">
-        Search
-      </Button>
-    </div>
+    </form>
   );
 }

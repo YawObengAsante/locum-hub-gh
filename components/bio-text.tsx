6e2982ed7ebafb7217c746fe7facaf8cee3b288a
Yaw Obeng Asante <yawobengasante@gmail.com>
2025-12-02 13:48:47 +0000

    (fix): bio text toggle button issue

--- components/bio-text.tsx
+++ components/bio-text.tsx
@@ -1,23 +1,24 @@
 "use client";
-import { Activity, useState } from "react";
+import { useState } from "react";
 
 export default function BioText({ bioText }: { bioText: string | null }) {
   const [isFullText, setIsFullText] = useState(false);
 
-  const handleClick = () => {
-    setIsFullText((prev) => !prev);
-  };
+  const maxLength = 120;
+  const isLong = bioText && bioText.length > maxLength;
+
   return (
     <p className="text-sm md:text-base text-gray-600">
-      {isFullText ? bioText : bioText?.substring(0, 120)}
-      <button
-        className="text-secondary hover:text-secondary/80 cursor-pointer text-sm ml-1 focus:outline-none"
-        onClick={handleClick}
-      >
-        <Activity mode={bioText ? "visible" : "hidden"}>
+      {isFullText || !isLong ? bioText : bioText?.substring(0, maxLength)}
+
+      {isLong && (
+        <button
+          className="text-secondary hover:text-secondary/80 cursor-pointer text-sm ml-1 focus:outline-none"
+          onClick={() => setIsFullText((prev) => !prev)}
+        >
           {isFullText ? "...show less" : "... show more"}
-        </Activity>
-      </button>
+        </button>
+      )}
     </p>
   );
 }

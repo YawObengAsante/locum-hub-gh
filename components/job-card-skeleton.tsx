f052deedfec07bc01e8b8670c4e38d30cf8d9448
Yaw Obeng Asante <yawobengasante@gmail.com>
2025-12-09 13:23:53 +0000

    (css): loading card border

--- components/job-card-skeleton.tsx
+++ components/job-card-skeleton.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "./ui/skeleton";
 
 export default function JobCardSkeleton() {
   return (
-    <div className="flex flex-col gap-y-3.5 w-full  gap-3">
+    <div className="flex flex-col gap-y-3.5 w-full gap-3 border border-border px-3 py-4 shadow-2xl rounded-2xl ">
       <div className="flex items-center space-x-4">
         <Skeleton className="h-12 w-12 rounded-full" />
         <div className="space-y-2">
@@ -11,7 +11,7 @@ export default function JobCardSkeleton() {
         </div>
       </div>
       <Skeleton className="h-4 w-[150px]" />
-      <Skeleton  className="h-4 w-[300px]"/>
+      <Skeleton className="h-4 w-[300px]" />
       <Skeleton className="h-30 w-[350px" />
     </div>
   );

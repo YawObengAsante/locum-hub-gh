320edd902113b30094e2022f6d07f5d579f89948
Yaw Obeng Asante <yawobengasante@gmail.com>
2026-06-01 21:39:12 +0000

    chore: job applicants modal complete

--- components/applicants-modal.tsx
+++ components/applicants-modal.tsx
@@ -1,5 +1,6 @@
 import { Dialog, DialogTitle, DialogTrigger, DialogContent } from "./ui/dialog";
 import { type UserType } from "@/types";
+import { FileTextIcon } from "lucide-react";
 
 export async function ApplicantsModal({
   applicants,
@@ -14,9 +15,30 @@ export async function ApplicantsModal({
       <DialogContent>
         <DialogTitle>Job Applicants</DialogTitle>
         {applicants.map((applicant) => (
-          <div key={applicant.id}>
-            <div className="text-lg font-semibold">{applicant.name}</div>
-            <div>{applicant.email}</div>
+          <div
+            key={applicant.id}
+            className="bg-muted/30 border border-border rounded-lg p-4 flex justify-between items-start"
+          >
+            <div className="grow">
+              <h3 className="font-semibold text-foreground mb-1">
+                {applicant.name}
+              </h3>
+              <p className="text-sm text-muted-foreground mb-2">
+                {applicant.email}
+              </p>
+            </div>
+            <div className="flex flex-col items-end gap-3">
+              <div className="flex flex-col gap-2">
+                <p className="text-sm text-accent hover:cursor-pointer flex items-center gap-1.5">
+                  Download CV
+                  <FileTextIcon size={15} />
+                </p>
+                <p className="text-sm text-accent hover:cursor-pointer flex items-center gap-1.5">
+                  Download Cover Letter
+                  <FileTextIcon size={15} />
+                </p>
+              </div>
+            </div>
           </div>
         ))}
       </DialogContent>
